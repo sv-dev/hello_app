d83cf65359ed43b1fff32adf496059b4785e2892
sv-dev <gertin7@gmail.com>
2022-05-06 13:40:36 +0300

    Fix: Solution 1.4.1 Ex. 1 - hola, mundo! - Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: '¡Hola, mundo!'
-  end
-
-  def goodbye
-    # render html: 'goodbye, world!'
     render html: 'hola, mundo!'
   end
 end
